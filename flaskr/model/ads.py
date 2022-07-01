bb93e92991d1a3a3bc36108654e18d4bc2e30553
이형준 <hyeongjune@ihyeongjun-ui-MacBookPro.local>
2022-07-01 14:12:20 +0900

    Fix: crud api

--- flaskr/model/ads.py
+++ flaskr/model/ads.py
@@ -1,6 +1,7 @@
-from pymongo import MongoClient
+from bson import json_util
+from pymongo import MongoClient, ReturnDocument
 import datetime
-from flask import current_app
+from flask import current_app, json, jsonify
 
 client = MongoClient('127.0.0.1', 27017)
 # client = MongoClient('mongodb://localhost:27017/')
@@ -12,12 +13,41 @@ ads_collection = db.test
 # collection = db[test]
 
 class FaceBookAds:
-    def __init__(self):
-        self.col = client['my_database']['test']
+    def __init__(self, db_name):
+        self.col = client['my_database'][db_name]
+
+    def _get_object(self):
+        pass
 
     def find_all(self):
-        print(self.col.fi)
-        return self.col.find()
+        db = self.col
+        ads = db.find()
+        response_data = []
+        for ad in ads:
+            response_data.append(ad)
+        #json.loads(json_util.dumps(response_data))
+        return response_data
+
+    def insert_data(self, **data):
+        db = self.col
+        result = db.insert_one(data)
+        return data
+
+    def find_one(self, name):
+        db = self.col
+        ad = db.find_one({'name':name})
+        return jsonify(ad)
+
+    def update_data(self, name, update_name):
+        db = self.col
+        ad = db.find_one_and_update({'name': name}, {'$set': {'name': update_name}}, return_document=ReturnDocument.AFTER)
+        return ad
+
+    def delete_data(self, name):
+        eb = self.col
+        ad = db.find_one_and_delete({'name': name})
+        return ad
+
 #document1 = {"name": "홍길동",
 #            "bio": "한국인입니다.",
 #            "tags": ["#몽고디비", "#파이썬", "#플라스크"],
--- flaskr/views/statistics.py
+++ flaskr/views/statistics.py
@@ -1,7 +1,7 @@
 from flask.views import MethodView
 from flask import jsonify, request
 
-from flaskr.model.ads import ads_collection
+from flaskr.model.ads import ads_collection, FaceBookAds
 
 
 class FacebookAdView(MethodView):
@@ -9,18 +9,20 @@ class FacebookAdView(MethodView):
         return ads_collection.find_one({'name':name})
 
     def get(self):
-        ads = ads_collection.find()
+        _ads = FaceBookAds('test')
+        ads = _ads.find_all()
         response_data = []
         for ad in ads:
             _response_data = {
                 'name': ad['name'],
             }
             response_data.append(_response_data)
-        return jsonify(response_data), 200
+        return jsonify({'result':response_data}), 200
 
     def post(self):
         request_data = request.json
-        ads_collection.insert_one({'name': request_data['name']})
+        db = FaceBookAds('test')
+        a = db.insert_data(**request_data)
         return jsonify({'result': 'success'}), 201
 
 
@@ -29,31 +31,27 @@ class FacebookAdDetailView(MethodView):
         return ads_collection.find_one({'name': name})
 
     def get(self, name):
+
         ad = self.get_object(name)
         if ad:
             response_data = {
                 'name': ad['name'],
-                'bio': ad['bio'],
-                'tags': ad['tags'],
-                'id': str(ad['_id']),
+                #'bio': ad['bio'],
+                #'tags': ad['tags'],
+                #'id': str(ad['_id']),
             }
             return jsonify(response_data), 200
         else:
             return jsonify({'error': 'data not found'}), 404
 
     def put(self, name):
-        ad = self.get_object(name)
-        request_data = request.json
-        if not ad:
-            return jsonify({'error': 'data not found'}), 404
-        else:
-            ads_collection.update_one({"name": name}, {"$set": {"name": request_data['name']}})
+        ad = FaceBookAds('test')
+        r_ad = ad.update_data('종이컵', '젖은 종이컵')
+        return jsonify(r_ad)
 
     def delete(self, name):
-        ad = self.get_object(name)
-
-        if not ad:
-            return jsonify({'error': 'data not found'}), 404
-        else:
-            ads_collection.delete_one({"name": name})
-            return jsonify({'message': 'delete success'}), 204
+        print(name)
+        ad = FaceBookAds('test')
+        d_ad = ad.delete_data(name)
+        print(d_ad)
+        return jsonify(d_ad)
